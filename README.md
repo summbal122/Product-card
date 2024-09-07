3274e04948dc857e93c8b1046d614e877a5a55e4
Sumbal Ijaz <sunbalijaz13@gmail.com>
2024-09-07 09:41:53 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Title
+Product Card
+
+## Features
+- CSS Flexbox
+- CSS Grid
+- HTML5 Markup
+- Responsive design with media queries
---
+++ scripts/card.js
@@ -0,0 +1,6 @@
+const element = document.getElementById('js-button');
+element.addEventListener('click', ()=>{
+  const buttonElement = document.querySelector('#js-button');
+  buttonElement.innerHTML = 'Added to cart';
+})
+ 
